d6ed8a8f55f8d146a2f034b98ff8c7bda7b09792
Joao Rangel <joao.vran@gmail.com>
2020-11-17 19:20:35 -0300

    refactored import to accept multiple files.

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,27 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    uploadedFiles.map(async uploadedfile => {
+      const data = new FormData();
 
-    // TODO
+      data.append('file', uploadedfile.file);
 
-    try {
-      // await api.post('/transactions/import', data);
-    } catch (err) {
-      // console.log(err.response.error);
-    }
+      try {
+        api.post('/transactions/import', data);
+      } catch (err) {
+        return console.log(err.response.error);
+      }
+      return history.push('/import', uploadedFiles);
+    });
+    return setUploadedFiles([]);
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const newFiles = files.map<FileProps>(file => {
+      return { file, name: file.name, readableSize: filesize(file.size) };
+    });
+
+    setUploadedFiles([...uploadedFiles, ...newFiles]);
   }
 
   return (
